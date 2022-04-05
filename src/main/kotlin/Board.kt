88a0aa065fc6763a944941ff0aa879560ad18f95
Irusha Sansuka <78542929+irusha@users.noreply.github.com>
2022-04-05 11:41:08 +0530

    Removed init() function as it is unnecessary. Game is now playable. But it is dumb. So you can easily win the game. Fix incoming

--- src/main/kotlin/Board.kt
+++ src/main/kotlin/Board.kt
@@ -1,37 +1,26 @@
-var row1a654651 = mutableListOf("","","")
-var row2a654651 = mutableListOf("","","")
-var row3a654651 = mutableListOf("","","")
-class Board{
+var row1a654651 = mutableListOf("   ", "   ", "   ")
+var row2a654651 = mutableListOf("   ", "   ", "   ")
+var row3a654651 = mutableListOf("   ", "   ", "   ")
 
-    fun init(){
-        var i = 0
-        while(i < 3){
-            var j = 0
-            while (j < 3){
-                editArrayValues(i,j,"   ")
-                j++
-            }
-            i++
-        }
-    }
+class Board {
+
     fun draw() {
         //this for loop is to get the row numbers
-        for (i in (0..2)){
+        for (i in (0..2)) {
             var str = ""
             //this for is to get the column number
-            for(j in (0..2)){
+            for (j in (0..2)) {
                 //check whether the column is the last column and print '|' if it isn't the last column
-                str = if(j != 2){
+                str = if (j != 2) {
                     //Set the values of the row strings according to the arrays ( X  O  X )
-                    "$str${getArrayValues(i,j)} | "
-                }
-                else{
+                    "$str${getArrayValues(i, j)} | "
+                } else {
                     //Set the values of the row strings according to the arrays and add the number of the row ( X  O  X   3)
-                    "$str${getArrayValues(i,j)}   ${3 - i}"
+                    "$str${getArrayValues(i, j)}   ${3 - i}"
                 }
             }
             println(str)
-            if(i != 2) {
+            if (i != 2) {
                 println("- - - - - - - -")
             }
         }
@@ -41,17 +30,16 @@ class Board{
 
     //set the value of a TicTacToe board. In here the mode is whether you add an X or O to the board. mode = true;
     //playing symbol = X
-    fun setSquareValue(coordinate:String, mode:Boolean):Boolean{
-        val mode1 = if(mode){
+    fun setSquareValue(coordinate: String, mode: Boolean): Boolean {
+        val mode1 = if (mode) {
             " X "
-        }
-        else{
+        } else {
             " O "
         }
         var rowNumber = 0
         var returnValue = true
         val chars = coordinate.split("")
-        val columnNumberForLetter = when(chars[1]){
+        val columnNumberForLetter = when (chars[1]) {
             "A", "a" -> 2
             "B", "b" -> 1
             "C", "c" -> 0
@@ -59,22 +47,20 @@ class Board{
         }
         try {
             rowNumber = 3 - chars[2].toInt()
-        }
-        catch (e: Exception){
+        } catch (e: Exception) {
             returnValue = false
         }
-        if(columnNumberForLetter == -1 || !returnValue){
+        if (columnNumberForLetter == -1 || !returnValue) {
             returnValue = false
-        }
-        else{
+        } else {
             editArrayValues(rowNumber, columnNumberForLetter, mode1)
         }
         return returnValue
     }
 
     //Set the value of the certain square of the board
-    private fun editArrayValues(row:Int, column:Int, data:String){
-        when(row){
+    private fun editArrayValues(row: Int, column: Int, data: String) {
+        when (row) {
             0 -> row1a654651[column] = data
             1 -> row2a654651[column] = data
             2 -> row3a654651[column] = data
@@ -82,9 +68,9 @@ class Board{
     }
 
     //Get the value of a certain square of the board
-    private fun getArrayValues(row:Int, column:Int):String{
+    private fun getArrayValues(row: Int, column: Int): String {
         var k = ""
-        when(row){
+        when (row) {
             0 -> k = row1a654651[column]
             1 -> k = row2a654651[column]
             2 -> k = row3a654651[column]
@@ -92,31 +78,31 @@ class Board{
         return k
     }
 
-    fun scanBoardForTheWinner():String{
+    fun scanBoardForTheWinner(): String {
         var returnValue = ""
-        for (i in (0..2)){
+        for (i in (0..2)) {
             //
-            val everyRow = getArrayValues(0,i) == getArrayValues(1, i) && getArrayValues(1, i) == getArrayValues(2, i)
-            val everyColumn = getArrayValues(i,0) == getArrayValues(i, 1) && getArrayValues(i, 1) == getArrayValues(i, 2)
+            val everyRow = getArrayValues(0, i) == getArrayValues(1, i) && getArrayValues(1, i) == getArrayValues(2, i)
+            val everyColumn =
+                getArrayValues(i, 0) == getArrayValues(i, 1) && getArrayValues(i, 1) == getArrayValues(i, 2)
             val noSpaceBoolColumn = getArrayValues(i, 2) != "   "
             val noSpaceBoolRow = getArrayValues(0, i) != "   "
-            val diagonalLeft = getArrayValues(0,0) == getArrayValues(1,1) && getArrayValues(1,1) == getArrayValues(2,2)
-            val noSpaceDiagonalBool = getArrayValues(1,1) != "   "
-            val diagonalRight = getArrayValues(0,2) == getArrayValues(1,1) && getArrayValues(2,0) == getArrayValues(1,1)
-            if(everyColumn && noSpaceBoolColumn){
+            val diagonalLeft =
+                getArrayValues(0, 0) == getArrayValues(1, 1) && getArrayValues(1, 1) == getArrayValues(2, 2)
+            val noSpaceDiagonalBool = getArrayValues(1, 1) != "   "
+            val diagonalRight =
+                getArrayValues(0, 2) == getArrayValues(1, 1) && getArrayValues(2, 0) == getArrayValues(1, 1)
+            if (everyColumn && noSpaceBoolColumn) {
                 returnValue = getArrayValues(i, 1)
                 break
-            }
-            else if(everyRow && noSpaceBoolRow){
+            } else if (everyRow && noSpaceBoolRow) {
                 returnValue = getArrayValues(1, i)
                 break
-            }
-            else if(diagonalRight && noSpaceDiagonalBool){
-                returnValue = getArrayValues(1,1)
+            } else if (diagonalRight && noSpaceDiagonalBool) {
+                returnValue = getArrayValues(1, 1)
                 break
-            }
-            else if(diagonalLeft && noSpaceDiagonalBool){
-                returnValue = getArrayValues(1,1)
+            } else if (diagonalLeft && noSpaceDiagonalBool) {
+                returnValue = getArrayValues(1, 1)
                 break
             }
 
@@ -124,26 +110,64 @@ class Board{
         return returnValue
     }
 
-    fun scanTwoSameSquares(): MutableList<String>{
+    fun scanTwoSameSquares(): MutableList<String> {
         val list = mutableListOf<String>()
+        var spaceCount = 0
+        var temporaryCoordinateStore = ""
+        if (scanBoardForTheWinner() == "") {
+            //For the row
+            for (i in (0..2)){
+                //For the column
+                for (j in (0..2)){
+                    if (getArrayValues(i,j) == "   "){
+                        spaceCount++
+                        temporaryCoordinateStore = coordinateConverter(i,j)
+                    }
+                }
+                if (spaceCount == 1){
+                    list.add(temporaryCoordinateStore)
+                }
+                spaceCount = 0
+                temporaryCoordinateStore = ""
 
+                for (j in (0..2)){
+                    if (getArrayValues(j,i) == "   "){
+                        spaceCount++
+                        temporaryCoordinateStore = coordinateConverter(j,i)
+                    }
+                }
+                if (spaceCount == 1){
+                    list.add(temporaryCoordinateStore)
+                }
+                spaceCount = 0
+                temporaryCoordinateStore = ""
+            }
+        }
         return list
     }
 
-    private fun coordinateConverter(row: Int, column: Int):String{
+    private fun coordinateConverter(row: Int, column: Int): String {
         val letter =
-        when(column){
-            0 -> "C"
-            1 -> "B"
-            2 -> "A"
-            else ->""
-        }
+            when (column) {
+                0 -> "C"
+                1 -> "B"
+                2 -> "A"
+                else -> ""
+            }
         return letter + (3 - row)
     }
 
-    fun scanEmptySquares(): MutableList<String>{
+    fun scanEmptySquares(): MutableList<String> {
         val list = mutableListOf<String>()
-
+        for (i in (0..2)) {
+            //for loop for the rows
+            for (j in (0..2)) {
+                //For loop for the column
+                if (getArrayValues(i, j) == "   ") {
+                    list.add(coordinateConverter(i, j))
+                }
+            }
+        }
         return list
     }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,25 @@
 fun main() {
     val board = Board()
-    board.init()
     board.draw()
     while(true) {
-        val b = readLine()
-        board.setSquareValue(b!!, false)
+        val emptySquares = board.scanEmptySquares()
+        board.setSquareValue(emptySquares[(0 until emptySquares.size).random()], true)
+        var j = board.scanBoardForTheWinner()
+        if (j != ""){
+            println("$j won")
+            break
+        }
         board.draw()
-        println(board.scanBoardForTheWinner())
+        var b = readLine()
+        while (!board.setSquareValue(b!!,false)){
+            println("Invalid coordinate")
+            b = readLine()!!
+        }
+        board.draw()
+        j = board.scanBoardForTheWinner()
+        if (j != ""){
+            println("$j won")
+            break
+        }
     }
 }
